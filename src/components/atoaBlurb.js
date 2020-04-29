058c5c0f351a5a8d2b6956ff3ed25cf3d5e91462
Patrick Seiuli <patrick.seiuli@gmail.com>
2020-04-28 21:35:53 -0600

    added image

--- src/components/atoaBlurb.js
+++ src/components/atoaBlurb.js
@@ -6,14 +6,7 @@ import { Link } from "gatsby"
 const AtoaBlurb = () => {
   const data = useStaticQuery(graphql`
     query {
-      flower: file(relativePath: { eq: "flower-mouth.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      fist: file(relativePath: { eq: "fist.png" }) {
+      landing: file(relativePath: { eq: "landing.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
@@ -24,22 +17,25 @@ const AtoaBlurb = () => {
   `)
   return (
     <div className="atoa-blurb">
-        <div className="inner-blurb">
-          <div className="content">
-            <h3>ATOA</h3>
-            <p>
-              Responsive web design project fighting the stigma around mental
-              health in the Polynesian community. Using Card Sorting, Tree
-              Tests, Click Tests, this app was created with users goals in mind.
-              The app was made responsive in order to reach a wider audience.{" "}
-            </p>
-          </div>
-          <div className="gallery">
-            <div className="column">
-              
-            </div>
-          </div>
+      <div className="inner-blurb">
+        <div className="content">
+          <h3>ATOA</h3>
+          <p>
+            Responsive web design project fighting the stigma around mental
+            health in the Polynesian community. Using Card Sorting, Tree Tests,
+            Click Tests, this app was created with users goals in mind. The app
+            was made responsive in order to reach a wider audience.
+          </p>
         </div>
+        <div className="btn-row">
+          <Link to="/work">View Series</Link>
+        </div>
+      </div>
+      <div className="images">
+        <div className="landing">
+          <Img fluid={data.landing.childImageSharp.fluid} />
+        </div>
+      </div>
     </div>
   )
 }
