9d2ea8470e88b89bae8bd4ea392472eb03c89ecc
Patrick Seiuli <patrick.seiuli@gmail.com>
2020-04-29 03:45:45 -0600

    added celebrity component

---
+++ src/components/celebrity.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
+
+const Celebrity = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      landing: file(relativePath: { eq: "landing.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div className="atoa-blurb">
+      <div className="inner-blurb">
+        <div className="content">
+          <h3>ATOA</h3>
+          <p>
+            Responsive web design project fighting the stigma around mental
+            health in the Polynesian community. Using Card Sorting, Tree Tests,
+            Click Tests, this app was created with users goals in mind. The app
+            was made responsive in order to reach a wider audience.
+          </p>
+          <div className="btn-row">
+            <Link to="/work">View Series</Link>
+          </div>
+        </div>
+        <div className="images">
+        <div className="landing">
+          <Img fluid={data.landing.childImageSharp.fluid} />
+        </div>
+      </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Celebrity
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/header"
 import Banner from "../components/banner"
 import AtoaBlurb from "../components/atoaBlurb"
 import Footer from "../components/footer"
+import Celebrity from "../components/celebrity.js"
 //scss
 import "../styles/styles.scss"
 // Normalize is optional up to you
@@ -14,6 +15,7 @@ const IndexPage = () => (
     <Header />
     <Banner />
     <AtoaBlurb />
+    <Celebrity />
     <Footer />
   </div>
 )
